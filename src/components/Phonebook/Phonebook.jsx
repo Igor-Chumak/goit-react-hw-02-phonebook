542b91d203a5a78960a33d09a761577f0143a0e8
Igor <125488812+Igor-Chumak@users.noreply.github.com>
2023-07-29 15:55:31 +0300

    css ok+

--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -31,7 +31,7 @@ export class Phonebook extends Component {
             maxlength="22"
             pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            placeholder=" "
+            placeholder=""
             required
           />
         </PhonebookLabel>
@@ -44,7 +44,7 @@ export class Phonebook extends Component {
             maxlength="17"
             pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            placeholder=" "
+            placeholder=""
             required
           />
         </PhonebookLabel>
