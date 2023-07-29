ef4697bc21c5413ba700dfa0eeada5fd945bd0e8
Igor <125488812+Igor-Chumak@users.noreply.github.com>
2023-07-29 21:19:30 +0300

    on Submit start

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,20 +1,15 @@
 import { Component } from 'react';
+import { nanoid } from 'nanoid';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyles, darkTheme, lightTheme, theme } from 'styles';
-import {
-  Header,
-  Section,
-  CreateThemeSwitcher,
-  ContactForm,
-  Contacts,
-} from 'components';
+import { Header, Section, CreateThemeSwitcher, ContactForm } from 'components';
 
-const INITIAL_STATE = [];
-//     {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-//     {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-//     {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-//     {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-//   ],
+const INITIAL_STATE = [
+  //     {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+  //     {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+  //     {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+  //     {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+];
 
 export class App extends Component {
   state = {
@@ -28,12 +23,6 @@ export class App extends Component {
       return {
         modeTheme: prevState.modeTheme === 'light' ? 'dark' : 'light',
       };
-    });
-  };
-
-  handleChangeInput = e => {
-    this.setState({
-      [name]: value,
     });
   };
 
