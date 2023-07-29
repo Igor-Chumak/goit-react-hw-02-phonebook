5abebbd8b43cc40ce724fbe8bd545e2d866ea9f1
Igor <125488812+Igor-Chumak@users.noreply.github.com>
2023-07-30 01:05:45 +0300

    filter css ok

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -2,20 +2,18 @@ import { Component } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-const FilterForm = styled.div`
-  max-width: ${props => props.theme.spacing(80)};
-  margin: 0 auto;
-  padding-bottom: ${props => props.theme.spacing(1)};
-  padding-left: ${props => props.theme.spacing(5)};
-  padding-right: ${props => props.theme.spacing(5)};
-  outline: 1px solid;
-`;
-
 const FilterLabel = styled.label`
   display: block;
+  margin-left: auto;
+  margin-right: auto;
   margin-bottom: ${props => props.theme.spacing(3)};
+  max-width: ${props => props.theme.spacing(80)};
+  padding-bottom: ${props => props.theme.spacing(2)};
+  padding-left: ${props => props.theme.spacing(5)};
+  padding-right: ${props => props.theme.spacing(5)};
   font-weight: 600;
   line-height: 1.5;
+  outline: 1px solid;
 `;
 
 const FilterInput = styled.input`
@@ -36,18 +34,16 @@ export class Filter extends Component {
 
   render() {
     return (
-      <FilterForm>
-        <FilterLabel>
-          Find contacts by name
-          <FilterInput
-            type="text"
-            name="filter"
-            maxLength="22"
-            placeholder=""
-            onChange={this.props.handleChangeInputFilter}
-          />
-        </FilterLabel>
-      </FilterForm>
+      <FilterLabel>
+        Find contacts by name
+        <FilterInput
+          type="text"
+          name="filter"
+          maxLength="22"
+          placeholder=""
+          onChange={this.props.handleChangeInputFilter}
+        />
+      </FilterLabel>
     );
   }
 }
