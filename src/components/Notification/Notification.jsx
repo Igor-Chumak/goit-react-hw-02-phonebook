720aeaab2f47ad7b7e235615a2b153447ca82156
Igor <125488812+Igor-Chumak@users.noreply.github.com>
2023-07-30 16:55:51 +0300

    note start

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -3,11 +3,13 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 const Message = styled.div`
+  width: 100%;
   color: inherit;
   font-weight: normal;
   font-size: 20px;
   text-transform: none;
   text-align: center;
+  background-color: ${props => props.theme.colors.alert};
 `;
 
 export class Notification extends Component {
